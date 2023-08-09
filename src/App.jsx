a5de68c37eaf8cae74bef92a5c0ed0a91c8f01c2
harshankrishdevbe <harshankrishdevbe@gmail.com>
2023-08-10 01:05:28 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import routes from "./routes/routes";
 import PageWrapper from "./components/common/PageWrapper";
-
+import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import "swiper/css";
 import "swiper/css/navigation";
